d9b2b037d8257301d74fee9c6f9423e85fa94aad
shen qin <shenqin@marketnode.com>
2023-09-11 11:53:23 +0800

    feat: init + add createElemnt

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './index.js', // 也可以叫index.js
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: `index.html`, //生成的文件名
+      template: path.resolve(__dirname, `index.html`), //源文件的绝对路径
+    }),
+  ],
+  mode: 'development',
+};
+
+module.exports = config;
